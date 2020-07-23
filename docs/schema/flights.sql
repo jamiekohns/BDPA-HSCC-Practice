b710c22c3322f6e852a7843ed6ece36a113c68cd
Jamie Kohns <jamiekohns@gmail.com>
2020-07-22 20:12:16 -0400

    Add Flights.sql schema and Tables-needed (for 7/23)

---
+++ docs/schema/flights.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `title` enum('M.','Ms.','Mrs.','Mr.') NOT NULL DEFAULT 'M.',
+  `middle_name` varchar(128) DEFAULT NULL,
+  `suffix` varchar(6) DEFAULT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `phone_number` varchar(32) DEFAULT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `security_question_1` varchar(255) NOT NULL,
+  `security_question_2` varchar(255) NOT NULL,
+  `security_question_3` varchar(255) NOT NULL,
+  `security_answer_1` varchar(255) NOT NULL,
+  `security_answer_2` varchar(255) NOT NULL,
+  `security_answer_3` varchar(255) NOT NULL,
+  `user_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `user_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(128) NOT NULL,
+  `zip` varchar(16) NOT NULL,
+  `country` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `users`
+    ADD CONSTRAINT `addresses_fk`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `addresses`(`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+
+ALTER TABLE `users`
+    ADD CONSTRAINT `user_type_fk`
+    FOREIGN KEY (`user_type_id`)
+    REFERENCES `user_type`(`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
