2c1e760b89c3648aa1553da870fc83ca34dccba0
unknown <saleemsaafir@gmail.com>
2020-07-25 11:50:03 -0400

    Made RestRequest class and optimized airports.php

--- app/src/RestRequest/RestRequest.php
+++ app/src/RestRequest/RestRequest.php
@@ -3,7 +3,28 @@
 namespace Flights\RestRequest;
 
 class RestRequest {
-    public function getName() {
-        return "I am RestRequest";
+    private $apiKey;
+    private $url;
+
+    public function __construct(string $apiKey, string $url){
+        $this->apiKey = $apiKey;
+        $this->url = $url;
+    }
+
+    public function send(){
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "key: $this->apiKey",
+            "content-type: application/json",
+        ]);
+
+        $response = curl_exec($ch);
+
+        return $response;
     }
 }
