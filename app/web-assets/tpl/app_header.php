65361b0556f4f6a4367fcd8b3a9bf94cdd10d9bd
unknown <saleemsaafir@gmail.com>
2020-07-30 19:02:52 -0400

    Added header template, footer, and nav-bar

---
+++ app/web-assets/tpl/app_header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            <?php if (isset($page_title)){ echo $page_title; } else { echo 'BDPA Flights';} ?>
+        </title>
+        <link rel="stylesheet" href="/web-assets/css/bootstrap.min.css">
+    </head>
+    <body class="bg-light">
