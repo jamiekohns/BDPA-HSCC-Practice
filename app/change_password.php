c97a5197cde0cda40d97dc1c15c7a553ef6bcbc0
Suchit Vemula <suchitvemula@localhost.attlocal.net>
2020-08-02 17:23:19 -0400

    added new files

---
+++ app/change_password.php
@@ -0,0 +1,59 @@
+<?php session_start();
+require 'init.php';
+use Flights\Database\User;
+
+if(isset($_SESSION['user']) && isset($_COOKIE['user'])){
+    header('location: user_dashboard.php');
+}
+if(!isset($_SESSION['forgot'])){
+    header('location: forgot_password.php');
+}
+$error = '';
+if(isset($_POST['submit'])){
+    if ($_POST['password'] == NULL){
+        $error = 'Please Enter Your Password';
+    } elseif ($_POST['password'] !== $_POST['password2']){
+        $error = 'The Two Passwords Do Not Match';
+    } else {
+        $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $user = new User();
+        $user->change_password($_SESSION['forgot_password']['first_name'], $_SESSION['forgot_password']['last_name'], $_SESSION['forgot_password']['email'], $password_hash);
+        $password_hash = NULL;
+        $_SESSION['forgot_password'] = NULL;
+        $_SESSION['forgot'] = NULL;
+        header('location: login.php');
+    }
+}
+?>
+
+<html>
+<head>
+    <title> Reset Password </title>
+    <script src="web-assets/js/jquery-3.5.1.min.js"></script>
+    <script src="web-assets/js/bootstrap.min.js"></script>
+    <link href="web-assets/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="web-assets/css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <?php
+        if ($error) {
+            echo '<div class="alert alert-danger">'.$error.'</div>';
+        }
+    ?>
+    <h1> Please Reset Password </h1>
+    <br>
+    <form action="change_password.php" method="post">
+
+        <div class="form-group col-md-6">
+            <label for="password"> Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="password"> Confirm Password</label>
+            <input type="password" class="form-control" id="password2" name="password2">
+        </div>
+        <div class="col-md-6">
+
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+       </div>
+    </form>
---
+++ app/forgot_password.php
@@ -0,0 +1,57 @@
+<?php session_start();
+require 'init.php';
+use Flights\Database\User;
+if(isset($_SESSION['user']) && isset($_COOKIE['user']) ){
+    header('location: user_dashboard.php');
+}
+$error = '';
+if(isset($_POST['submit'])){
+    if ($_POST['first_name'] == NULL){
+        $error = 'Please Enter Your First Name';
+    } elseif ($_POST['last_name'] == NULL){
+        $error = 'Please Enter Your Last Name';
+    } elseif ($_POST['email'] == NULL){
+        $error = 'Please Enter Your Email Address';
+    } else {
+        $user = new User();
+        $user->get_questions($_POST['first_name'], $_POST['last_name'], $_POST['email']);
+        header('location: answer_questions.php');
+    }
+
+}
+
+?>
+<html>
+
+<head>
+    <title> Forgot Password </title>
+    <script src="web-assets/js/jquery-3.5.1.min.js"></script>
+    <script src="web-assets/js/bootstrap.min.js"></script>
+    <link href="web-assets/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="web-assets/css/style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+        if ($error) {
+            echo '<div class="alert alert-danger">'.$error.'</div>';
+        }
+    ?>
+    <form action="forgot_password.php" method="post">
+        <div class="form-group col-md-6">
+            <label for="first_name">First Name</label>
+            <input type="text" class="form-control" id="first_name" name="first_name">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="first_name">Last Name</label>
+            <input type="text" class="form-control" id="last_name" name="last_name">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="col-md-6">
+
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+       </div>
+    </form>
