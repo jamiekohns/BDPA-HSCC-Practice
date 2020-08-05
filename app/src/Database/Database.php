2377a3ab26847dff667e767e5cc96262062d12bd
Suchit Vemula <suchitvemula@localhost.attlocal.net>
2020-08-04 20:43:06 -0400

    Database

--- app/src/Database/Database.php
+++ app/src/Database/Database.php
@@ -1,13 +1,21 @@
 <?php
+
 namespace Flights\Database;
 
-use PDO;
 class Database {
     protected $db;
 
-    public function __construct(){
+    public function __construct() {
+        $dsn = sprintf(
+            'mysql:dbname=@;host=%2;port=3306',
+            $_ENV['DB_NAME'],
+            $_ENV['DB_HOST']
+        );
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
+
         try {
-            $this->db = new PDO($_ENV['DSN'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+            $this->db = new PDO($dsn, $user, $password);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
         }
