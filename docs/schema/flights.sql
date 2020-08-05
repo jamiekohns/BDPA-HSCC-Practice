fc994e651ee7b309fb3a5f808e2810cd016aa836
Suchit Vemula <suchitvemula@localhost.attlocal.net>
2020-08-04 21:19:42 -0400

    Schema

--- docs/schema/flights.sql
+++ docs/schema/flights.sql
@@ -2,10 +2,10 @@
 -- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 29, 2020 at 01:32 AM
+-- Host: localhost
+-- Generation Time: Aug 05, 2020 at 03:18 AM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,6 +36,36 @@ CREATE TABLE `addresses` (
   `zip` varchar(16) NOT NULL,
   `country` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `addresses`
+--
+
+INSERT INTO `addresses` (`id`, `address`, `city`, `state`, `zip`, `country`) VALUES
+(1, '1980 shoal crest way', 'cumming', 'GA', '30041', 'USA'),
+(4, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(5, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(6, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(7, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(8, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(9, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(10, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(11, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(12, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(13, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(14, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(15, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(16, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(17, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(18, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(19, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(20, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(21, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(22, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(23, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(24, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(25, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
+(26, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States');
 
 -- --------------------------------------------------------
 
@@ -101,19 +131,27 @@ CREATE TABLE `users` (
   `title` enum('M.','Ms.','Mrs.','Mr.') NOT NULL DEFAULT 'M.',
   `middle_name` varchar(128) DEFAULT NULL,
   `suffix` varchar(6) DEFAULT NULL,
-  `dob` date NOT NULL,
-  `gender` varchar(1) NOT NULL,
-  `address_id` int(11) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  `address_id` int(11) DEFAULT NULL,
   `phone_number` varchar(32) DEFAULT NULL,
-  `email_address` varchar(255) NOT NULL,
-  `security_question_1` varchar(255) NOT NULL,
-  `security_question_2` varchar(255) NOT NULL,
-  `security_question_3` varchar(255) NOT NULL,
-  `security_answer_1` varchar(255) NOT NULL,
-  `security_answer_2` varchar(255) NOT NULL,
-  `security_answer_3` varchar(255) NOT NULL,
-  `user_type_id` int(11) NOT NULL
+  `email_address` varchar(255) DEFAULT NULL,
+  `security_question_1` varchar(255) DEFAULT NULL,
+  `security_question_2` varchar(255) DEFAULT NULL,
+  `security_question_3` varchar(255) DEFAULT NULL,
+  `security_answer_1` varchar(255) DEFAULT NULL,
+  `security_answer_2` varchar(255) DEFAULT NULL,
+  `security_answer_3` varchar(255) DEFAULT NULL,
+  `user_type_id` int(11) NOT NULL,
+  `confirmed` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `password_hash`, `title`, `middle_name`, `suffix`, `dob`, `gender`, `address_id`, `phone_number`, `email_address`, `security_question_1`, `security_question_2`, `security_question_3`, `security_answer_1`, `security_answer_2`, `security_answer_3`, `user_type_id`, `confirmed`) VALUES
+(14, 'Suchit', 'Vemula', '$2y$10$fGflYzC1CWQhAVJEGUix2uwGFvPMJJUJAaAmO.NeaajjkcqxH2mVu', 'M.', 'm.', 'jr.', '2020-05-13', 'M', 26, '4078088396', 'vemulasuchit3@gmail.com', '1+1', '1+1', '1+1', '2', '2', '2', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -138,6 +176,14 @@ CREATE TABLE `user_type` (
   `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`id`, `name`) VALUES
+(1, 'Costumer'),
+(2, 'Administrator');
 
 --
 -- Indexes for dumped tables
@@ -195,6 +241,12 @@ ALTER TABLE `user_type`
 --
 
 --
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
 -- AUTO_INCREMENT for table `flight_status`
 --
 ALTER TABLE `flight_status`
@@ -207,10 +259,22 @@ ALTER TABLE `payments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT for table `user_log`
 --
 ALTER TABLE `user_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
