63b50d30a53aafc0205f9455481737f1d9556dff
Suchit Vemula <suchitvemula@localhost.attlocal.net>
2020-08-05 18:28:24 -0400

    Added link on navbar

--- app/web-assets/tpl/app_nav.php
+++ app/web-assets/tpl/app_nav.php
@@ -1,7 +1,7 @@
 
 <div class="container col-md-11 mt-3 sticky-top mb-1">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark rounded">
-        <span class="navbar-brand mb-0 h1">BDPA Flights</span>
+        <a  href="index.php"class="navbar-brand mb-0 h1">BDPA Flights</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
