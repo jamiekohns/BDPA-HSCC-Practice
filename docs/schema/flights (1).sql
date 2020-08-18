de8525d390d3a9ff62cc3221d03c17113d92f4e2
NewTigerShark10 <68254961+NewTigerShark10@users.noreply.github.com>
2020-08-18 19:17:33 -0400

    Update flights (1).sql

--- docs/schema/flights (1).sql
+++ docs/schema/flights (1).sql
@@ -41,92 +41,7 @@ CREATE TABLE `addresses` (
 -- Dumping data for table `addresses`
 --
 
-INSERT INTO `addresses` (`id`, `address`, `city`, `state`, `zip`, `country`) VALUES
-(1, '1980 shoal crest way', 'cumming', 'GA', '30041', 'USA'),
-(4, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(5, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(6, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(7, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(8, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(9, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(10, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(11, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(12, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(13, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(14, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(15, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(16, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(17, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(18, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(19, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(20, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(21, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(22, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(23, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(24, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(25, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(26, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(27, '1980 shoal crest way', 'cumming', 'GA', '30041', 'United States'),
-(28, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(29, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(30, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(31, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(32, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(33, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(34, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(35, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(36, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(37, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(38, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(39, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(40, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(41, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(42, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(43, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(44, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(45, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(46, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(47, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(48, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(49, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(50, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(51, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(52, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(53, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(54, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(55, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(56, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(57, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(58, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(59, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(60, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(61, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(62, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(63, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(64, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(65, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(66, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(67, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(68, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(69, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(70, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(71, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(72, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(73, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(74, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(75, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(76, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(77, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(78, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(79, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(80, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(81, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(82, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(83, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(84, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(85, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(86, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US'),
-(87, '1980 shoal crest way', 'cumming', 'GA', '30041', 'US');
+
 
 -- --------------------------------------------------------
 
@@ -167,12 +82,7 @@ CREATE TABLE `payments` (
 -- Dumping data for table `payments`
 --
 
-INSERT INTO `payments` (`id`, `card_number`, `expiration_date`, `cvc`, `cardholder_name`, `address_id`, `user_id`) VALUES
-(2, '123', '2020-08-06', '123', 'Suchit', 56, NULL),
-(5, '123', '2020-08-13', '123', 'Suchit', 64, NULL),
-(9, '1123', '2020-08-20', '123', 'Suchit', 82, 14),
-(10, '123', '2020-08-12', '123', 'Suchit', 84, NULL),
-(11, '123', '2020-08-12', '123', 'Suchit', 86, NULL);
+
 
 -- --------------------------------------------------------
 
@@ -203,12 +113,6 @@ CREATE TABLE `tickets` (
 -- Dumping data for table `tickets`
 --
 
-INSERT INTO `tickets` (`id`, `first_name`, `middle_name`, `last_name`, `gender`, `dob`, `phone_number`, `email_address`, `payment_id`, `seat_assignment`, `checkin_bags`, `carryon_bags`, `flight_id`, `status_id`, `user_id`, `address_id`) VALUES
-(11, 'Suchit', 'm.', 'Vemula', 'M', '2020-08-12', '4078088396', 'vemulasuchit3@gmail.com', 5, 'A1', 1, 1, '5f2342e09c4d62d7b65d83ad', 1, NULL, 65),
-(15, 'Suchit', 'm.', 'Vemula', 'M', '2020-08-05', '4078088396', 'vemulasuchit3@gmail.com', 9, 'B1', 1, 1, '5f2342e09c4d62d7b65d83ad', 1, 14, 83),
-(17, 'Suchit', 'm.', 'Vemula', 'M', '2020-08-12', '4078088396', 'vemulasuchit3@gmail.com', 11, 'A8', 1, 1, '5f2342e09c4d62d7b65d83ad', 1, NULL, 87);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -241,12 +145,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `password_hash`, `title`, `middle_name`, `suffix`, `dob`, `gender`, `address_id`, `phone_number`, `email_address`, `security_question_1`, `security_question_2`, `security_question_3`, `security_answer_1`, `security_answer_2`, `security_answer_3`, `user_type_id`, `confirmed`) VALUES
-(14, 'Suchit', 'Vemula', '$2y$10$fGflYzC1CWQhAVJEGUix2uwGFvPMJJUJAaAmO.NeaajjkcqxH2mVu', 'M.', 'm.', 'jr.', '2020-05-13', 'M', 26, '4078088396', 'vemulasuchit3@gmail.com', '1+1', '1+1', '1+1', '2', '2', '2', 2, 1),
-(15, 'Suchit3', 'Vemula', '$2y$10$uigRDGiYV..m3rMkCA4uX.9/zL.ase1fYE4cDU6t4wTtUOJRI7YEC', 'M.', 'm.', NULL, NULL, NULL, NULL, NULL, 'vemulasuchit3@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, 1, 0),
-(16, 'Suchit2', 'Vemula2', '$2y$10$5AJOOQ4BW2I2j7eiSZ3Uru5tX.oOa9N7Asb7FO23zj/vY95zYCHHu', 'M.', 'm.2', 'jr.', '2020-08-06', 'M', 27, '4078088396', 'vemulasuchit3@gmail.com', '1+1', '1+1', '1+1', '2', '2', '2', 1, 1);
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `user_log`
